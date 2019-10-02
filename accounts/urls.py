1ad33502d79efdad28e72c4372432591e56ad1ef
speedyray <speedyray2ray@gmail.com>
2019-10-02 17:14:58 -0400

    Added detail views and upvotes

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('', views.hello, name='hello'),
+    path('', views.hello, name='hello'),
     path('signup', views.signup, name='signup'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),

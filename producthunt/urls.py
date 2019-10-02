a6f95a433ca380a5fdff8eaecc394db1ffe0f82e
speedyray <speedyray2ray@gmail.com>
2019-10-02 08:58:59 -0400

    Add create products

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -16,12 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from products import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('products/', include('products.urls')),
     path('accounts/', include('accounts.urls')),
     path('signup/', include('accounts.urls')),
     path('login/', include('accounts.urls')),
     path('logout/', include('accounts.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
